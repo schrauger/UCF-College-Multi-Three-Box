a99ec548a203ac4f03c72f1bca1015ea5ed6d457
Stephen Schrauger <stephen@schrauger.com>
2019-02-11 09:19:48 -0500

    initial commit

---
+++ includes/acf-pro-fields.php
@@ -0,0 +1,244 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: stephen
+ * Date: 2019-02-01
+ * Time: 1:47 PM
+ */
+class ucf_college_multi_three_box_acf_pro_fields {
+
+    const shortcode = 'ucf_college_multi_three_box';
+
+    function __construct() {
+        add_action( 'acf/init', array( 'ucf_college_multi_three_box_acf_pro_fields', 'create_fields' ) );
+    }
+
+    static function create_fields() {
+        if ( function_exists( 'acf_add_local_field_group' ) ) {
+            acf_add_local_field_group(
+                array(
+                    'key'                   => 'group_5c59f155c5f05',
+                    'title'                 => 'Multi Three Box',
+                    'fields'                => array(
+                        array(
+                            'key' => 'field_5c59f2f12fd06',
+                            'label' => 'Header',
+                            'name' => 'header',
+                            'type' => 'text',
+                            'instructions' => '',
+                            'required' => 0,
+                            'conditional_logic' => 0,
+                            'wrapper' => array(
+                                'width' => '',
+                                'class' => '',
+                                'id' => '',
+                            ),
+                            'default_value' => '',
+                            'placeholder' => '',
+                            'prepend' => '',
+                            'append' => '',
+                            'maxlength' => '',
+                        ),
+                        array(
+                            'key'               => 'field_5c59f15ac5579',
+                            'label'             => 'Triple Box Row',
+                            'name'              => 'triple_box_row',
+                            'type'              => 'repeater',
+                            'instructions'      => '',
+                            'required'          => 0,
+                            'conditional_logic' => 0,
+                            'wrapper'           => array(
+                                'width' => '',
+                                'class' => '',
+                                'id'    => '',
+                            ),
+                            'collapsed'         => '',
+                            'min'               => 1,
+                            'max'               => 0,
+                            'layout'            => 'table',
+                            'button_label'      => '',
+                            'sub_fields'        => array(
+                                array(
+                                    'key'               => 'field_5c59f1de18314',
+                                    'label'             => 'Box 1',
+                                    'name'              => 'box_1',
+                                    'type'              => 'group',
+                                    'instructions'      => '',
+                                    'required'          => 0,
+                                    'conditional_logic' => 0,
+                                    'wrapper'           => array(
+                                        'width' => '',
+                                        'class' => '',
+                                        'id'    => '',
+                                    ),
+                                    'layout'            => 'block',
+                                    'sub_fields'        => array(
+                                        array(
+                                            'key'               => 'field_5c59f20118315',
+                                            'label'             => 'Box Text',
+                                            'name'              => 'box_text',
+                                            'type'              => 'text',
+                                            'instructions'      => '',
+                                            'required'          => 0,
+                                            'conditional_logic' => 0,
+                                            'wrapper'           => array(
+                                                'width' => '',
+                                                'class' => '',
+                                                'id'    => '',
+                                            ),
+                                            'default_value'     => '',
+                                            'placeholder'       => '',
+                                            'prepend'           => '',
+                                            'append'            => '',
+                                            'maxlength'         => '',
+                                        ),
+                                        array(
+                                            'key'               => 'field_5c59f20c18316',
+                                            'label'             => 'Box URL',
+                                            'name'              => 'box_url',
+                                            'type'              => 'url',
+                                            'instructions'      => '',
+                                            'required'          => 0,
+                                            'conditional_logic' => 0,
+                                            'wrapper'           => array(
+                                                'width' => '',
+                                                'class' => '',
+                                                'id'    => '',
+                                            ),
+                                            'default_value'     => '',
+                                            'placeholder'       => '',
+                                        ),
+                                    ),
+                                ),
+                                array(
+                                    'key'               => 'field_5c59f21f18317',
+                                    'label'             => 'Box 2',
+                                    'name'              => 'box_2',
+                                    'type'              => 'group',
+                                    'instructions'      => '',
+                                    'required'          => 0,
+                                    'conditional_logic' => 0,
+                                    'wrapper'           => array(
+                                        'width' => '',
+                                        'class' => '',
+                                        'id'    => '',
+                                    ),
+                                    'layout'            => 'block',
+                                    'sub_fields'        => array(
+                                        array(
+                                            'key'               => 'field_5c59f21f18318',
+                                            'label'             => 'Box Text',
+                                            'name'              => 'box_text',
+                                            'type'              => 'text',
+                                            'instructions'      => '',
+                                            'required'          => 0,
+                                            'conditional_logic' => 0,
+                                            'wrapper'           => array(
+                                                'width' => '',
+                                                'class' => '',
+                                                'id'    => '',
+                                            ),
+                                            'default_value'     => '',
+                                            'placeholder'       => '',
+                                            'prepend'           => '',
+                                            'append'            => '',
+                                            'maxlength'         => '',
+                                        ),
+                                        array(
+                                            'key'               => 'field_5c59f21f18319',
+                                            'label'             => 'Box URL',
+                                            'name'              => 'box_url',
+                                            'type'              => 'url',
+                                            'instructions'      => '',
+                                            'required'          => 0,
+                                            'conditional_logic' => 0,
+                                            'wrapper'           => array(
+                                                'width' => '',
+                                                'class' => '',
+                                                'id'    => '',
+                                            ),
+                                            'default_value'     => '',
+                                            'placeholder'       => '',
+                                        ),
+                                    ),
+                                ),
+                                array(
+                                    'key'               => 'field_5c59f2221831a',
+                                    'label'             => 'Box 3',
+                                    'name'              => 'box_3',
+                                    'type'              => 'group',
+                                    'instructions'      => '',
+                                    'required'          => 0,
+                                    'conditional_logic' => 0,
+                                    'wrapper'           => array(
+                                        'width' => '',
+                                        'class' => '',
+                                        'id'    => '',
+                                    ),
+                                    'layout'            => 'block',
+                                    'sub_fields'        => array(
+                                        array(
+                                            'key'               => 'field_5c59f2221831b',
+                                            'label'             => 'Box Text',
+                                            'name'              => 'box_text',
+                                            'type'              => 'text',
+                                            'instructions'      => '',
+                                            'required'          => 0,
+                                            'conditional_logic' => 0,
+                                            'wrapper'           => array(
+                                                'width' => '',
+                                                'class' => '',
+                                                'id'    => '',
+                                            ),
+                                            'default_value'     => '',
+                                            'placeholder'       => '',
+                                            'prepend'           => '',
+                                            'append'            => '',
+                                            'maxlength'         => '',
+                                        ),
+                                        array(
+                                            'key'               => 'field_5c59f2221831c',
+                                            'label'             => 'Box URL',
+                                            'name'              => 'box_url',
+                                            'type'              => 'url',
+                                            'instructions'      => '',
+                                            'required'          => 0,
+                                            'conditional_logic' => 0,
+                                            'wrapper'           => array(
+                                                'width' => '',
+                                                'class' => '',
+                                                'id'    => '',
+                                            ),
+                                            'default_value'     => '',
+                                            'placeholder'       => '',
+                                        ),
+                                    ),
+                                ),
+                            ),
+                        ),
+                    ),
+                    'location'              => array(
+                        array(
+                            array(
+                                'param'    => 'post_taxonomy',
+                                'operator' => '==',
+                                'value'    => 'ucf_shortcode_category:' . self::shortcode,
+                            ),
+                        ),
+                    ),
+                    'menu_order'            => 0,
+                    'position'              => 'normal',
+                    'style'                 => 'default',
+                    'label_placement'       => 'top',
+                    'instruction_placement' => 'label',
+                    'hide_on_screen'        => '',
+                    'active'                => 1,
+                    'description'           => '',
+                ) );
+
+        }
+    }
+}
+
+new ucf_college_multi_three_box_acf_pro_fields();
---
+++ includes/ucf-college-multi-three-box-shortcode.php
@@ -0,0 +1,107 @@
+<?php
+
+class ucf_college_multi_three_box_shortcode {
+    const shortcode_slug = 'ucf_college_multi_three_box'; // the shortcode text entered by the user (inside square brackets)
+    const shortcode_name = 'Multi Three Box';
+    const shortcode_description = 'Tabbed sections with repeater list';
+    //const get_param_group = 'people_group'; // group or category person is in
+
+    function __construct() {
+        add_action( 'init', array( $this, 'add_shortcode' ) );
+        add_filter( 'query_vars', array($this, 'add_query_vars_filter' )); // tell wordpress about new url parameters
+    }
+
+    /**
+     * Adds the shortcode to wordpress' index of shortcodes
+     */
+    function add_shortcode() {
+        if ( ! ( shortcode_exists( self::shortcode_slug ) ) ) {
+            add_shortcode( self::shortcode_slug, array($this, 'replacement' ));
+        }
+    }
+
+    /**
+     * Tells wordpress to listen for the 'people_group' parameter in the url. Used to filter down to specific profiles.
+     * @param $vars
+     *
+     * @return array
+     */
+    function add_query_vars_filter($vars){
+        //$vars[] = self::get_param_group;
+        return $vars;
+    }
+
+    /**
+     * Returns the replacement html that WordPress uses in place of the shortcode
+     *
+     * @return mixed
+     */
+    function replacement( ) {
+        $replacement_data = ''; //string of html to return
+
+        if ( have_rows( 'triple_box_row' ) ) {
+            $header = get_field( 'header' );
+            $replacement_data .= "
+<div class='container'>
+    <h2>{$header}</h2>
+    <div class='grid-box'>";
+
+            // print out each row of 3 boxes
+            while ( have_rows( 'triple_box_row' ) ) {
+                the_row();
+
+                for ( $i = 1; $i <= 3; $i ++ ) {
+                    $acf_individual_box_group_id = 'box_' . $i;
+                    if ( have_rows( $acf_individual_box_group_id ) ) {
+
+                        // print out each individual box
+                        while ( have_rows( $acf_individual_box_group_id ) ) {
+                            the_row();
+                            $box_text = get_sub_field( 'box_text' );
+                            $box_url  = get_sub_field( 'box_url' );
+                            $replacement_data .= "
+    <a class='{$acf_individual_box_group_id}' href='{$box_url}'><span>{$box_text}</span></a>";
+                        }
+                    }
+                }
+
+
+            }
+            $replacement_data .= "
+</div>";
+
+        }
+        return $replacement_data;
+    }
+
+    /**
+     * Only run this on plugin activation, as it's stored in the database
+     */
+    static function insert_shortcode_term(){
+        $taxonomy = new ucf_college_shortcode_taxonomy;
+        $taxonomy->create_taxonomy();
+        wp_insert_term(
+            self::shortcode_name,
+            ucf_college_shortcode_taxonomy::taxonomy_slug,
+            array(
+                'description' => self::shortcode_description,
+                'slug' => self::shortcode_slug
+            )
+        );
+    }
+
+    /**
+     * Run when plugin is disabled and/or uninstalled. This removes the shortcode from the list of shortcodes in the taxonomy.
+     */
+    static function delete_shortcode_term(){
+        $taxonomy = new ucf_college_shortcode_taxonomy;
+        $taxonomy->create_taxonomy();
+        wp_delete_term(get_term_by('slug', self::shortcode_slug)->term_id, ucf_college_shortcode_taxonomy::taxonomy_slug);
+    }
+
+
+
+
+}
+
+new ucf_college_multi_three_box_shortcode();
