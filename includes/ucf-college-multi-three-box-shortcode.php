f5fb245c7d2ccc8cce1c224f6eaa44c5aa4843d3
Stephen Schrauger <stephen@schrauger.com>
2019-02-12 16:28:23 -0500

    multiple styles; backgrounds

--- includes/ucf-college-multi-three-box-shortcode.php
+++ includes/ucf-college-multi-three-box-shortcode.php
@@ -1,15 +1,16 @@
 <?php
 
 class ucf_college_multi_three_box_shortcode {
-    const shortcode_slug = 'ucf_college_multi_three_box'; // the shortcode text entered by the user (inside square brackets)
-    const shortcode_name = 'Multi Three Box';
+    const shortcode_slug        = 'ucf_college_multi_three_box'; // the shortcode text entered by the user (inside square brackets)
+    const shortcode_name        = 'Multi Three Box';
     const shortcode_description = 'Tabbed sections with repeater list';
+
     //const get_param_group = 'people_group'; // group or category person is in
 
     function __construct() {
         add_action( 'init', array( $this, 'add_shortcode' ) );
-        add_filter( 'query_vars', array($this, 'add_query_vars_filter' )); // tell wordpress about new url parameters
-        add_filter( 'ucf_college_shortcode_menu_item', array($this, 'add_ckeditor_shortcode'));
+        add_filter( 'query_vars', array( $this, 'add_query_vars_filter' ) ); // tell wordpress about new url parameters
+        add_filter( 'ucf_college_shortcode_menu_item', array( $this, 'add_ckeditor_shortcode' ) );
     }
 
     /**
@@ -17,29 +18,31 @@ class ucf_college_multi_three_box_shortcode {
      */
     function add_shortcode() {
         if ( ! ( shortcode_exists( self::shortcode_slug ) ) ) {
-            add_shortcode( self::shortcode_slug, array($this, 'replacement' ));
+            add_shortcode( self::shortcode_slug, array( $this, 'replacement' ) );
         }
     }
 
     /**
      * Adds the shortcode to the ckeditor dropdown menu
      */
-    function add_ckeditor_shortcode($shortcode_array){
+    function add_ckeditor_shortcode( $shortcode_array ) {
         $shortcode_array[] = array(
-            'slug' => self::shortcode_slug,
-            'name' => self::shortcode_name,
+            'slug'        => self::shortcode_slug,
+            'name'        => self::shortcode_name,
             'description' => self::shortcode_description
         );
+
         return $shortcode_array;
     }
 
     /**
      * Tells wordpress to listen for the 'people_group' parameter in the url. Used to filter down to specific profiles.
+     *
      * @param $vars
      *
      * @return array
      */
-    function add_query_vars_filter($vars){
+    function add_query_vars_filter( $vars ) {
         //$vars[] = self::get_param_group;
         return $vars;
     }
@@ -49,7 +52,23 @@ class ucf_college_multi_three_box_shortcode {
      *
      * @return mixed
      */
-    function replacement( ) {
+    function replacement() {
+        switch ( get_field( 'style_selection' ) ) {
+
+            case 'image': // style 1 - image boxes
+                return $this->replacement_image_boxes();
+                break;
+            case 'icon': // style 2 - small icon boxes
+            default:
+                return $this->replacement_small_icon_boxes();
+
+        }
+    }
+
+    /**
+     *
+     */
+    function replacement_image_boxes() {
         $replacement_data = ''; //string of html to return
 
         if ( have_rows( 'triple_box_row' ) ) {
@@ -70,10 +89,22 @@ class ucf_college_multi_three_box_shortcode {
                         // print out each individual box
                         while ( have_rows( $acf_individual_box_group_id ) ) {
                             the_row();
-                            $box_text = get_sub_field( 'box_text' );
-                            $box_url  = get_sub_field( 'box_url' );
+                            $box_text             = get_sub_field( 'box_text' );
+                            $box_url              = get_sub_field( 'box_url' );
+                            $background_image_id = get_sub_field( 'background_image_id' );
+                            $size = 'thumbnail';
+                            $background_image = wp_get_attachment_image_src($background_image_id, $size);
+                            $background_image_url = $background_image[0];
+
                             $replacement_data .= "
-    <a class='{$acf_individual_box_group_id}' href='{$box_url}'><span>{$box_text}</span></a>";
+    <a 
+    class='{$acf_individual_box_group_id}' 
+    style='background-image: url(\"{$background_image_url}\")' 
+    href='{$box_url}'
+    >
+            
+        <span>{$box_text}</span>
+    </a>";
                         }
                     }
                 }
@@ -84,13 +115,69 @@ class ucf_college_multi_three_box_shortcode {
 </div>";
 
         }
+
         return $replacement_data;
     }
 
     /**
+     *
+     */
+    function replacement_small_icon_boxes() {
+        $replacement_data = ''; //string of html to return
+
+        if ( have_rows( 'triple_box_row' ) ) {
+            $header = get_field( 'header' );
+            $replacement_data .= "
+<div class='wide-box'>
+    <div class='wide-box-overlay'></div>
+    <div class='container'>";
+
+            // print out each row of 3 boxes
+            while ( have_rows( 'triple_box_row' ) ) {
+                the_row();
+
+                for ( $i = 1; $i <= 3; $i ++ ) {
+                    $acf_individual_box_group_id = 'box_' . $i;
+                    if ( have_rows( $acf_individual_box_group_id ) ) {
+
+                        // print out each individual box
+                        while ( have_rows( $acf_individual_box_group_id ) ) {
+                            the_row();
+                            $box_text             = get_sub_field( 'box_text' );
+                            $box_url              = get_sub_field( 'box_url' );
+                            $background_image_id = get_sub_field( 'background_image_id' );
+                            $size = 'thumbnail';
+                            $background_image = wp_get_attachment_image_src($background_image_id, $size);
+                            $background_image_url = $background_image[0];
+
+                            $replacement_data .= "
+        <div class='inner-box {$acf_individual_box_group_id}'>
+            <a href='{$box_url}'>
+                <img src='{$background_image_url}' />
+                <br />
+                <span>{$box_text}</span>
+            </a>
+        </div>";
+                        }
+                    }
+                }
+
+
+            }
+            $replacement_data .= "
+    </div>
+</div>";
+
+        }
+
+        return $replacement_data;
+    }
+
+
+    /**
      * Only run this on plugin activation, as it's stored in the database
      */
-    static function insert_shortcode_term(){
+    static function insert_shortcode_term() {
         $taxonomy = new ucf_college_shortcode_taxonomy;
         $taxonomy->create_taxonomy();
         wp_insert_term(
@@ -98,21 +185,20 @@ class ucf_college_multi_three_box_shortcode {
             ucf_college_shortcode_taxonomy::taxonomy_slug,
             array(
                 'description' => self::shortcode_description,
-                'slug' => self::shortcode_slug
+                'slug'        => self::shortcode_slug
             )
         );
     }
 
     /**
-     * Run when plugin is disabled and/or uninstalled. This removes the shortcode from the list of shortcodes in the taxonomy.
+     * Run when plugin is disabled and/or uninstalled. This removes the shortcode from the list of shortcodes in the
+     * taxonomy.
      */
-    static function delete_shortcode_term(){
+    static function delete_shortcode_term() {
         $taxonomy = new ucf_college_shortcode_taxonomy;
         $taxonomy->create_taxonomy();
-        wp_delete_term(get_term_by('slug', self::shortcode_slug)->term_id, ucf_college_shortcode_taxonomy::taxonomy_slug);
+        wp_delete_term( get_term_by( 'slug', self::shortcode_slug )->term_id, ucf_college_shortcode_taxonomy::taxonomy_slug );
     }
-
-
 
 
 }
